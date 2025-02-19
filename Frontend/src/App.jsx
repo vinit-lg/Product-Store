1a5e183baf4e7e07291237aa0eaf1d32e46bd8a1
vinit kumar <vinitkr622@gmail.com>
2025-02-19 12:17:04 +0530

    changes in page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Box, useColorModeValue  } from '@chakra-ui/react'
 import React from 'react'
 import { Route, Routes} from 'react-router-dom'
 import HomePage from './pages/HomePage'
-import CreatePage from './pages/createPage'
+import CreatePage from './pages/CreatePage'
 import Navbar from './components/Navbar'
 
 
