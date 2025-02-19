62f6f41b535f0a5ca076550bc82dfaa5d24c7b84
vinit kumar <vinitkr622@gmail.com>
2025-02-19 11:50:45 +0530

    Project Completed

---
+++ Backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import path from "path";
+
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+const __dirname = path.resolve();
+
+app.use(express.json());
+
+// Routes
+app.use("/api/products", productRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
+
+app.listen(PORT, ()=> {
+    connectDB();
+    console.log("Server at http://localhost:" + PORT);
+});
---
+++ Frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { Box, useColorModeValue  } from '@chakra-ui/react'
+import React from 'react'
+import { Route, Routes} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/createPage'
+import Navbar from './components/Navbar'
+
+
+
+const App = () => {
+  return (
+  <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+    {<Navbar /> }
+    <Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/create" element={<CreatePage />} />
+    </Routes>
+  </Box>
+  )
+}
+
+export default App
